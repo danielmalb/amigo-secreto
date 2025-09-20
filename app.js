c5e7eb45db6f097ba08dfc74850c1ba28477af4c
Daniel Albuquerque <danielmalb@gmail.com>
2025-09-20 07:24:05 -0400

    Subindo para o Github, o código referente a criação do Jogo Amigo Secreto, desenvolvido por Daniel Albuquerque

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+
+    if (amigos.includes(nome)) {
+        alert("Esse amigo(a) já foi adicionado.");
+        input.value = "";
+        return;
+    }
+
+    amigos.push(nome);
+
+    atualizarLista();
+
+    input.value = "";
+}
+
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((amigo, index) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+
+        const btnRemover = document.createElement("button");
+        btnRemover.textContent = "❌";
+        btnRemover.style.marginLeft = "10px";
+        btnRemover.onclick = () => removerAmigo(index);
+
+        li.appendChild(btnRemover);
+        lista.appendChild(li);
+    });
+}
+
+function removerAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Adicione pelo menos o nome de dois amigos para realizar o sorteio.");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `O amigo secreto sorteado é: <strong>${amigoSorteado}</strong></li>`;
+}
